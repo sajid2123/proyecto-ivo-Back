17d78f383762c5948682cc00f33b4f1b65370221
Sajid Ahmad <sajidmalhi9@gmail.com>
2024-02-03 17:23:37 +0100

    Modelos,Contraladores,Factories  del Proyecto

---
+++ app/Http/Controllers/AdministrativoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Administrativo;
+use Illuminate\Http\Request;
+
+class AdministrativoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Administrativo $administrativo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Administrativo $administrativo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Administrativo $administrativo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Administrativo $administrativo)
+    {
+        //
+    }
+}
