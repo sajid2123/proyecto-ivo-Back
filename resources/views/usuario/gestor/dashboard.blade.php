ae283c41cb748703a2f9deab56bd536483d8d9fa
Sajid Ahmad <sajidmalhi9@gmail.com>
2024-02-11 18:41:33 +0100

    Sajid 11/02/2024 18:39

--- resources/views/usuario/gestor/dashboard.blade.php
+++ resources/views/usuario/gestor/dashboard.blade.php
@@ -1,37 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
-</head>
-<body>
-    <div class="row">
-        <div class="col-2">
-            <h1>dsadasda</h1>
-        </div>
-        <div class="col-10">
+<!-- bootstrap  -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<!-- Fontawesome  -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- css  -->
+<link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 
+<div class="container-fluid p-0 font">
+    <div class="row">
+        <div class="col-2 p-0">
+            @include('usuario.gestor.nav')
+        </div>
+        <div class="col-10 p-0 px-5">
+            <div class="row mt-4">
+                <div class="col-12">
+                    <h1 class="title">Usuarios</h1>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-6">
+                     <h1>Medico</h1>
+                </div>
+                <div class="col-6 d-flex justify-content-end">
+                    <a href="" class="btn-add-user">Add Usuario</a>
+                </div>
+            </div>
         </div>
     </div>
+</div>
 
-
-
-
-
-
-
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.9/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
-
-<h1>Aqui va dashboard</h1>
-
-<h1>Nombre Usuario: {{Auth::guard('usuario')->user()->nombre}}</h1>
-
-
-<a href="{{route('usuario.logout')}}">Logout</a>
