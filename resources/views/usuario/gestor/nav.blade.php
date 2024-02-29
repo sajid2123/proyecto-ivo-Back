2019f93aac63d6b682537c2268c8e67919f0a0bf
GMP22 <gemepe28@gmail.com>
2024-02-29 19:33:30 +0100

    blade

--- resources/views/usuario/gestor/nav.blade.php
+++ resources/views/usuario/gestor/nav.blade.php
@@ -6,9 +6,9 @@
 <!-- Css  -->
 <link rel="stylesheet" href="{{ asset('css/nav.css') }}">
 
-<div class="container-fluid font ht-100">
+<div class="container-fluid font h-100">
         <div class="row">
-            <div class="col-2 position-fixed nav-style ">
+            <div class="col-2 position-fixed nav-style">
                 <div class="row pt-5">
                     <div class="col-12 text-center">
                         <img src="{{ asset('img/logoIvoBlanco.png') }}" alt="Logo" width="100px" heigth="100px">
@@ -37,18 +37,51 @@
                         </ul>
                     </div>
                 </div>  
-                <div class="row mt">
-                    <div class="col-12  d-flex align-items-end justify-content-center ">
-                            <a href="" class="a-tag d-flex flex-column mx-4">
-                                <span class="nombre">{{Auth::guard('usuario')->user()->nombre}}</span>
-                                <span class="correo">{{Auth::guard('usuario')->user()->correo}}</span>
-                            </a>
-                            <a href="{{route('usuario.logout')}}" class="a-tag logout-icon">
-                                <i class="fa-solid fa-arrow-right-from-bracket"></i>
-                            </a>
+
+                <div class="row d-flex h-50 align-items-end">
+                    <div class="col-12  d-flex justify-content-center ">
+                            <div class="d-flex justify-content-center ">
+                                <p class="mb-2 letra-login">{{Auth::guard('usuario')->user()->nombre[0]}}</p>
+                            </div>
+                            <div class="d-flex justify-content-center "> 
+                                <div class="a-tag d-flex flex-column mx-4">
+                                    <span class="nombre">{{Auth::guard('usuario')->user()->nombre}}</span>
+                                    <span class="correo">{{Auth::guard('usuario')->user()->correo}}</span>
+                                </div>
+                            </div>
+                           <div class="d-flex justify-content-center ">
+                                <a href="{{route('usuario.logout')}}" class="a-tag logout-icon">
+                                    <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                                </a>
+                           </div>
+
                     </div>
-                
                 </div>
+
+                <!--
+                <div class="row mt">
+                <div class="row justify-content-cente">
+                    <div class="col-11">
+                         <div class="col-8 ms-3 d-flex align-items-center justify-content-between mx-auto">
+                            <div>
+                                <p class="mx-2 me-3 letra-login">{{Auth::guard('usuario')->user()->nombre[0]}}</p>
+                            </div>
+                            <div class="d-flex flex-column justify-content-start ">
+                                <p style="font-weight: 500; text-align: start;">{{Auth::guard('usuario')->user()->nombre}}</p>
+                                <p style="font-weight: 200;">{{Auth::guard('usuario')->user()->correo}}</p>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-center ms-4">
+                                <a href="{{route('usuario.logout')}}" class="a-tag logout-icon">
+                                    <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                                </a>
+                           </div>
+          
+                        </div>
+                    </div>
+                </div>
+
+            </div>-->
+
             </div>
         </div>
 </div>
